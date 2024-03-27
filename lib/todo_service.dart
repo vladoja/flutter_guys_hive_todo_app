e06b71b9cc1a4b26be90e1d4ea249a8819e0cf67
vladoja <dr.jalo2@gmail.com>
2024-03-27 21:38:10 +0100

    CRUD: addItem, getAllTodos.

--- lib/todo_service.dart
+++ lib/todo_service.dart
@@ -7,4 +7,15 @@ class TodoService {
 
   Future<Box<TodoItem>> get _box async =>
       await Hive.openBox<TodoItem>(_boxName);
+
+  Future<int> addItem(TodoItem item) async {
+    var box = await _box;
+    final int itemId = await box.add(item);
+    return itemId;
+  }
+
+  Future<List<TodoItem>> getAllTodos() async {
+    final box = await _box;
+    return box.values.toList();
+  }
 }
